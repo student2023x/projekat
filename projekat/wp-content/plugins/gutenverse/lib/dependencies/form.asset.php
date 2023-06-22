c9b909f161d4af5613dc5c5f6b14d6ff5f2b279e
student2023x <projectstud2023@gmail.com>
2023-06-23 01:41:50 +0200

    Poslednja ispravka
    Ispravljene greške u radu.

---
+++ projekat/wp-content/plugins/gutenverse/lib/dependencies/form.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'ff1472c575289f7285dc24eeba694fe3');
---
+++ projekat/wp-content/plugins/gutenverse/lib/dependencies/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-hooks', 'wp-polyfill', 'wp-url'), 'version' => '39c58544b9b13df3c2e0bb4de76f9511');
